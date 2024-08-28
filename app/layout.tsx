d26d715b93982839990acab341a9fcaddc62f960
myrddral <thirdeye0@gmail.com>
2024-08-28 22:25:14 +0200

    extended site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,32 +1,55 @@
 import type { PropsWithChildren } from 'react'
 import type { Metadata } from 'next'
 
+import { siteConfig } from '@/lib/site-config'
 import { glysa, lexendDeca } from '@/lib/fonts/fonts'
 import { ThemeProvider } from '@/components/theme-provider'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'UX Studio Challenge | Attila Béli',
-  description: 'This is a solution to the UX Studio Challenge by Attila Béli.',
-  creator: 'Attila Béli',
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
   openGraph: {
     type: 'website',
-    url: 'https://uxstudio-challenge.vercel.app/',
-    title: 'UX Studio Challenge | Attila Béli',
-    description: 'This is a solution to the UX Studio Challenge by Attila Béli.',
-    siteName: 'UX Studio Challenge',
+    locale: siteConfig.locale,
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
     images: [
       {
-        url: 'https://dynamic-og-image-generator.vercel.app/api/generate?title=UX+Studio+Challenge&author=Attila+B%C3%A9li&websiteUrl=https%3A%2F%2Fuxstudio-challenge.vercel.app%2F&avatar=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcRlQE4Q7T5S7zR47fVlpqJmElUdobrdkbrZRg%26s&theme=default',
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
       },
     ],
   },
+  twitter: {
+    card: 'summary_large_image',
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [siteConfig.ogImage],
+  },
+  robots: 'noindex, nofollow',
+  authors: [
+    {
+      name: 'Attila Béli',
+      url: 'https://github.com/myrddral',
+    },
+  ],
+  creator: 'Attila Béli',
 }
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
-      <body className={`${lexendDeca.variable} ${glysa.variable} font-body text-body`}>
+      <body
+        className={`${lexendDeca.variable} ${glysa.variable} mx-auto max-w-screen-2xl font-body text-body`}
+      >
         <ThemeProvider attribute="class" disableTransitionOnChange>
           {children}
         </ThemeProvider>
---
+++ lib/site-config.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: 'UX Studio Challenge Solution | Attila Béli',
+  description: 'This is a solution to the UX Studio Challenge by Attila Béli.',
+  url: 'https://uxstudio-challenge.vercel.app/',
+  creator: 'Attila Béli - https://www.attilabeli.com',
+  ogImage:
+    'https://dynamic-og-image-generator.vercel.app/api/generate?title=UX+Studio+Challenge&author=Attila+B%C3%A9li&websiteUrl=https%3A%2F%2Fuxstudio-challenge.vercel.app%2F&avatar=https%3A%2F%2Fencrypted-tbn0.gstatic.com%2Fimages%3Fq%3Dtbn%3AANd9GcRlQE4Q7T5S7zR47fVlpqJmElUdobrdkbrZRg%26s&theme=default',
+  locale: 'en_US',
+  htmlLang: 'en',
+}
+
+export type SiteConfig = typeof siteConfig
