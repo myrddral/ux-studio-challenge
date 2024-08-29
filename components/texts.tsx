efd273f3bc2ac1192effbb69993109d562e08248
myrddral <thirdeye0@gmail.com>
2024-08-29 05:41:03 +0200

    using cn caused a bug with the primitive text components, removed

--- components/texts.tsx
+++ components/texts.tsx
@@ -1,21 +1,19 @@
 import type { PropsWithChildren } from 'react'
 
-import { cn } from '@/lib/utils'
-
 function Title({ children, className }: PropsWithChildren<{ className?: string }>) {
-  return <h1 className={cn('font-heading text-h1', className)}>{children}</h1>
+  return <h1 className={`font-heading text-h1 ${className}`}>{children}</h1>
 }
 
 function Subtitle({ children, className }: PropsWithChildren<{ className?: string }>) {
-  return <h2 className={cn('font-heading text-h2', className)}>{children}</h2>
+  return <h2 className={`font-heading text-h2 ${className}`}>{children}</h2>
 }
 
 function Highlight({ children, className }: PropsWithChildren<{ className?: string }>) {
-  return <p className={cn('text-highlight font-body', className)}>{children}</p>
+  return <p className={`text-highlight font-body ${className}`}>{children}</p>
 }
 
 function Message({ children, className }: PropsWithChildren<{ className?: string }>) {
-  return <p className={cn('font-body text-message', className)}>{children}</p>
+  return <p className={`font-body text-message ${className}`}>{children}</p>
 }
 
 export { Title, Subtitle, Highlight, Message }
