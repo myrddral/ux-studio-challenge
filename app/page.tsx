c3eb6060b52a3937a6de7cacd86c93fc42afa202
myrddral <thirdeye0@gmail.com>
2024-09-01 22:10:25 +0200

    added automatic redirect to /contacts (until menus implemented)

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
-import { Title } from "@/components/texts";
+// import { Title } from '@/components/texts'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center p-6 md:p-24">
-      <Title>UX Studio Challenge</Title>
-    </main>
-  )
+  redirect('/contacts')
+
+  // return (
+  //   <main className="flex min-h-screen flex-col items-center p-6 md:p-24">
+  //     <Title className="duration-500 animate-in fade-in slide-in-from-top-10">UX Studio Challenge</Title>
+  //   </main>
+  // )
 }
