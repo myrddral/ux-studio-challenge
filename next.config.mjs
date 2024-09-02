212a6069c9b5145cf7eab652b9347b6962d08fa6
myrddral <thirdeye0@gmail.com>
2024-09-02 12:59:46 +0200

    fixed invalid image pathname causing build error in next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,13 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'ux-studio-challenge-portraits.s3.eu-central-1.amazonaws.com',
         port: '',
-        pathname: '',
+        pathname: '/',
       },
     ],
     minimumCacheTTL: 1800,
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@aws-sdk/client-s3', '@aws-sdk/s3-request-presigner'],
   },
 }
 
