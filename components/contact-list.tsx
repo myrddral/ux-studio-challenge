a765faaac419438f996019fc62e04d48a6970df4
myrddral <thirdeye0@gmail.com>
2024-09-03 12:52:40 +0200

    added react query to handle s3 image url fetching

--- components/contact-list.tsx
+++ components/contact-list.tsx
@@ -1,25 +1,27 @@
 import type { Contact } from '@/lib/schemas/contact.schema'
 
 import { ContactListItem } from './contact-list-item'
-import { useEffect, useState } from 'react'
 import { getImageUrls } from '@/app/s3-actions'
+import { useQuery } from '@tanstack/react-query'
+import { Loading } from './ui/loading'
 
 export function ContactList({ contacts }: { contacts: Contact[] }) {
-  const [avatarUrls, setAvatarUrls] = useState<string[]>([])
+  const { data: avatarUrls = [], isLoading } = useQuery({
+    queryKey: ['get-image-urls', contacts],
+    queryFn: async () => {
+      const keys = contacts.map((contact) => contact.avatar).filter(Boolean)
+      return await getImageUrls(keys)
+    },
+    enabled: contacts.length > 0,
+  })
 
-  useEffect(() => {
-    const keys = contacts.map((contact) => `${contact.avatar?.split('.')[0]}_thumbnail.webp`)
-    getImageUrls(keys)
-      .then((urls) => setAvatarUrls(urls))
-      .catch((err) => console.error('Error fetching avatar URLs:', err))
-  }, [contacts])
-
+  if (isLoading) return <Loading />
   if (!contacts.length) return 'No contacts found'
 
   return (
     <ul className="flex flex-col">
       {contacts.map((contact, index) => (
-        <ContactListItem key={contact.id} contact={{ ...contact, avatar: avatarUrls[index] }} />
+        <ContactListItem key={contact.id} contact={{ ...contact, avatar: avatarUrls[index] || '' }} />
       ))}
     </ul>
   )
