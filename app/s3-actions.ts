08c6987a4447a64959c3927aee3bef6f47a68088
myrddral <thirdeye0@gmail.com>
2024-09-05 21:08:58 +0200

    added mock auth to s3 related server actions

--- app/s3-actions.ts
+++ app/s3-actions.ts
@@ -1,11 +1,24 @@
 'use server'
-import { s3, GetObjectCommand, getSignedUrl, PutObjectCommand } from '@/lib/aws/aws-s3-client'
+import type { GetObjectCommandInput, PutObjectCommandInput } from '@aws-sdk/client-s3'
+import { s3, GetObjectCommand, PutObjectCommand, getSignedUrl } from '@/lib/aws/aws-s3-client'
+import { auth } from '@/lib/auth/auth'
 
-//TODO: check if its worth doing batch updates with several keys of contactlist
+/**
+ * Retrieves the signed URL for an image from S3.
+ *
+ * @param key The key of the image to retrieve
+ * @returns The signed URL to retrieve the image from S3
+ */
 export const getImageUrl = async (key: string) => {
-  const getParams = { Bucket: process.env.S3_BUCKET_NAME, Key: key }
+  const { userId } = auth()
+
+  if (!userId) throw new Error('Unauthorized')
 
   try {
+    const getParams: GetObjectCommandInput = {
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: key,
+    }
     const command = new GetObjectCommand(getParams)
     const url = await getSignedUrl(s3, command, { expiresIn: 3600 })
     return url
@@ -15,14 +28,26 @@ export const getImageUrl = async (key: string) => {
   }
 }
 
+/**
+ * Retrieves the signed URLs for multiple images from S3.
+ *
+ * @param keys An array of keys of the images to retrieve
+ * @returns An array of signed URLs to retrieve the images from S3
+ */
 export const getImageUrls = async (keys: (string | null)[]) => {
-  const getParams = { Bucket: process.env.S3_BUCKET_NAME }
+  const { userId } = auth()
+
+  if (!userId) throw new Error('Unauthorized')
 
   try {
     const urls = await Promise.all(
       keys.map(async (key) => {
         if (!key) return null
-        const command = new GetObjectCommand({ ...getParams, Key: key as string })
+        const getParams: GetObjectCommandInput = {
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: key,
+        }
+        const command = new GetObjectCommand(getParams)
         return await getSignedUrl(s3, command, { expiresIn: 3600 })
       })
     )
@@ -33,11 +58,24 @@ export const getImageUrls = async (keys: (string | null)[]) => {
   }
 }
 
-export const getUploadUrl = async (key: string) => {
-  const getParams = { Bucket: process.env.S3_BUCKET_NAME, Key: key }
+/**
+ * Retrieves the signed URL for uploading an image to S3.
+ *
+ * @param key The key of the image to upload
+ * @returns The signed URL to upload the image to S3
+ */
+export const getImageUploadUrl = async (key: string) => {
+  const { userId } = auth()
+
+  if (!userId) throw new Error('Unauthorized')
+
+  const putParams: PutObjectCommandInput = {
+    Bucket: process.env.S3_BUCKET_NAME,
+    Key: key,
+  }
 
   try {
-    const command = new PutObjectCommand(getParams)
+    const command = new PutObjectCommand(putParams)
     const url = await getSignedUrl(s3, command, { expiresIn: 3600 })
     return url
   } catch (err) {
